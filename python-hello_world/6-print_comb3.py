f666abd8b875b157885100b6f843ababdb0caa36
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-19 02:01:36 +0100

    numbers combiation

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,13 +1,3 @@
-# i = range(10)
-# j = range(10)
-# for i in range(10):
-#     for j in range(10):
-#         if i < 10: 
-#             print("{:01d}{}".format(i,j), end=", ")
-#         #     i +=1
-#         # else:
-#         #     print("{}{}".format(i,j), end="\n")
-
 for i in range(0, 10):
         for j in range(i + 1, 10):
             combination = str(i) + str(j)
