f6e344b84edcd7dcb1ad194b19283519bbafb70d
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-19 01:11:57 +0100

    missed space added

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 i = range(100)
 for i in range(100):
     if i <99: 
-        print("{:02d}".format(i), end=",")
+        print("{:02d}".format(i), end=", ")
         i +=1
     else:
         print("{:02d}".format(i), end="\n")
