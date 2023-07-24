922151d88f3eb71beba8182d2b3f4a61371b65c3
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-24 23:12:29 +0100

    Implemented temp convert function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 def convert_to_celsius(fahrenheit):
     celcius = ((fahrenheit-32)*(5/9))
-    if celcius < -273.15 or celcius > 273.15:
+    if celcius < -273.15:
         return -273.15
     elif celcius > 273.15:
         return 273.15
