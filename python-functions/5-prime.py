8bdcefffe4c525a685c1ce0041162d98deb4f801
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-26 00:18:07 +0100

    fixed edge case of prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,9 @@
 def is_prime(number):
     n = (number - 1) % 6
     m = (number + 1) % 6
-    if number in (0,2,3):
+    if number <= 0:
+        return False
+    elif number == 2 or 3:
         return True
     elif n and m == 0:
         return True
