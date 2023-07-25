67c81569358a8506404407ca3b5d7807d75b9b19
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-25 01:03:33 +0100

    Fixed fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,11 @@
 def fibonacci_sequence(n):
-    sequence = [0,1]
+    sequence = []
     if n == 0 or None:
-        return([])
+        return(sequence)
     else:
+        (sequence.append(0))
+        (sequence.append(1))
         while len(sequence) < n:
             (sequence.append(sequence[-1] + sequence[-2]))
         return sequence
+print(fibonacci_sequence(20))
