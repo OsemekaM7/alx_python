1747df7a17963fe7d37286a061d00fdf993d2e58
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-25 21:47:52 +0100

    fibo

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,8 @@
 def fibonacci_sequence(n):
     sequence = []
-    if n == 0:
+    if n <= 0:
         return([]) #For cases where n is 0, list must be empty
-    elif n < 2:
+    elif n == 1:
         return([0]) #For cases where n is 1, list has one element such that len(sequence) == n
     else:
         (sequence.append(0))
@@ -11,3 +11,4 @@ def fibonacci_sequence(n):
             (sequence.append(sequence[-1] + sequence[-2]))
 
     return sequence
+print(fibonacci_sequence(-1))
