e47e774680873a5ea1178c8bb35a570c941c4b80
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-26 20:16:11 +0100

    Changed the logic of checks

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,9 @@
 def is_prime(number):
     n = (number - 1) % 6
     m = (number + 1) % 6
-    if number == 2 or number == 3:
+    if number <= 0:
+        return False
+    elif number <= 3:
         return True
     elif n == 0 or m  == 0:
         return True
