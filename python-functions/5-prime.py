31256bc538b1ef0f2f0fc3fde3b22c2104e3cc04
OsemekaM7 <osemeka@DESKTOP-SRLK5NO>
2023-07-26 20:02:43 +0100

    Okay

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,7 @@
 def is_prime(number):
     n = (number - 1) % 6
     m = (number + 1) % 6
-    if number == 2 or 3:
+    if number == 2 or number == 3:
         return True
     elif n or m  == 0:
         return False
