f299d9d9f904e0db35676de72498b2e49aef0c2c
OsemekaM7 <molosemart365@gmail.com>
2023-08-22 23:41:17 +0100

    initial commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@ def square_matrix_simple(matrix):
     for row in matrix:
         new_matrix = []
         for i, num in enumerate(row):
-            new_matrix.append(num ** 2)
+            new_matrix.append([num ** 2])
         print(new_matrix)
 
-print(square_matrix_simple([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+# print(square_matrix_simple([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
