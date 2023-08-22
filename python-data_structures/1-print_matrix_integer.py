fe058f75f22898f9d6d484713af2b7731d86a80a
OsemekaM7 <molosemart365@gmail.com>
2023-08-22 20:17:35 +0100

    matrices

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,8 @@
 def print_matrix_integer(matrix):
     for row in matrix:
+        row_str = ""
         for i, num in enumerate(row):
-            if i < len(row) - 1:
-                print("{:d}".format(num), end=" ")
-            else:
-                print("{:d}".format(num))
-        print()
+            if i > 0:
+                row_str += " "
+            row_str += "{:d}".format(num)
+        print(row_str)
