e0719392ff7be7394d8c19e37999995cb6acef3c
OsemekaM7 <molosemart365@gmail.com>
2023-08-23 07:30:22 +0100

    get highest value

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,17 @@
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+            return None
+    
+    first_key = next(iter(a_dictionary))
+    first_value  = a_dictionary[first_key]
+    
+    greater_keys = [key for key, value in a_dictionary.items() if value > first_value]
+    
+    if not greater_keys:
+        return first_value
+    
+    best_key = max(greater_keys, key=lambda key: (a_dictionary[key], key))
+    highest_score = a_dictionary[best_key]
+    return highest_score
+
+print (best_score({'Alex': 23, 'Ginny': 45, 'Georgina': 14}))
