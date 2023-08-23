6101bc49542b10df4d3d59359d164adf54ee8691
OsemekaM7 <molosemart365@gmail.com>
2023-08-23 07:55:52 +0100

    get highest value - a million debugs later

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,17 +1,17 @@
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if not a_dictionary:
             return None
     
     first_key = next(iter(a_dictionary))
     first_value  = a_dictionary[first_key]
     
-    greater_keys = [key for key, value in a_dictionary.items() if value > first_value]
+    greater_keys = [key for key, value in a_dictionary.items() if value is not None and (first_value is None or value > first_value)]
     
     if not greater_keys:
         return first_key
     
-    best_key = max(greater_keys, key=lambda key: (a_dictionary[key], key))
+    best_key = max(greater_keys)
     highest_score = a_dictionary[best_key]
     return best_key
 
-# print (best_score({'Alex': 23, 'Ginny': 45, 'Georgina': 14}))
+# print (best_score({'Alex': None, 'Ginny': 45, 'Georgina': 14}))
