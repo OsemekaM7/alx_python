eb119837c9a2ae5ada589e18e0b96831a39fa043
OsemekaM7 <molosemart365@gmail.com>
2023-08-26 11:35:10 +0100

    fix args function

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,17 +5,17 @@ def main():
 
     num_args = len(argv)
     if num_args == 0:
-        print("Number of argument(s): 0.")
+        print("0 arguments")
         print(".")
     else:
-        print(f"Number of argument(s): {num_args}.")
+        print("{} arguments".format(num_args))
         if num_args == 1:
             print("Argument:")
         else:
             print("Arguments:")
         
         for i, arg in enumerate(argv, start=1):
-            print(f"{i}: {arg}")
+            print('{}: {}'.format(i, arg))
 
 if __name__ == "__main__":
     main()
